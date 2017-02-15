a63e94a047411cd11c765ecde65a1edf6f8e9397
Rui Maranhao <rui@computer.org>
2017-02-15 23:06:06 +0000

    improve BankException.java: a custom exception class does not inherit non-default constructors

--- bank/src/main/java/pt/ulisboa/tecnico/softeng/bank/exception/BankException.java
+++ bank/src/main/java/pt/ulisboa/tecnico/softeng/bank/exception/BankException.java
@@ -1,5 +1,7 @@
 package pt.ulisboa.tecnico.softeng.bank.exception;
 
 public class BankException extends RuntimeException {
-
+   public BankException(String message) {
+      super(message);
+   }
 }
