85be1a4e0f83b080b12ec4525e5f80ee7f1b0f37
António Rito Silva <Rito.Silva@ist.utl.pt>
2017-03-18 21:26:33 +0000

    test success sequence of Adventure process method

---
+++ broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/AdventureSequenceTest.java
@@ -0,0 +1,93 @@
+package pt.ulisboa.tecnico.softeng.broker.domain;
+
+import org.joda.time.LocalDate;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import mockit.Injectable;
+import mockit.Mocked;
+import mockit.StrictExpectations;
+import mockit.integration.junit4.JMockit;
+import pt.ulisboa.tecnico.softeng.broker.domain.Adventure.State;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.ActivityInterface;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.BankInterface;
+import pt.ulisboa.tecnico.softeng.broker.interfaces.HotelInterface;
+import pt.ulisboa.tecnico.softeng.hotel.domain.Room.Type;
+
+@RunWith(JMockit.class)
+public class AdventureSequenceTest {
+	private static final String IBAN = "BK01987654321";
+	private static final int AMOUNT = 300;
+	private static final int AGE = 20;
+	private static final String PAYMENT_CONFIRMATION = "PaymentConfirmation";
+	private static final String PAYMENT_CANCELLATION = "PaymentCancellation";
+	private static final String ACTIVITY_CONFIRMATION = "ActivityConfirmation";
+	private static final String ACTIVITY_CANCELLATION = "ActivityCancellation";
+	private static final String ROOM_CONFIRMATION = "RoomConfirmation";
+	private static final String ROOM_CANCELLATION = "RoomCancellation";
+	private static final LocalDate arrival = new LocalDate(2016, 12, 19);
+	private static final LocalDate departure = new LocalDate(2016, 12, 21);
+
+	@Injectable
+	private Broker broker;
+
+	@Test
+	public void successSequenceOne(@Mocked final BankInterface bankInterface,
+			@Mocked final ActivityInterface activityInterface, @Mocked final HotelInterface roomInterface) {
+		new StrictExpectations() {
+			{
+				BankInterface.processPayment(IBAN, AMOUNT);
+				this.result = PAYMENT_CONFIRMATION;
+
+				ActivityInterface.reserveActivity(arrival, departure, AGE);
+				this.result = ACTIVITY_CONFIRMATION;
+
+				HotelInterface.reserveRoom(Type.SINGLE, arrival, departure);
+				this.result = ROOM_CONFIRMATION;
+
+				BankInterface.getOperationData(PAYMENT_CONFIRMATION);
+
+				ActivityInterface.getActivityReservationData(ACTIVITY_CONFIRMATION);
+
+				HotelInterface.getRoomBookingData(ROOM_CONFIRMATION);
+			}
+		};
+
+		Adventure adventure = new Adventure(this.broker, arrival, departure, AGE, IBAN, AMOUNT);
+
+		adventure.process();
+		adventure.process();
+		adventure.process();
+		adventure.process();
+
+		Assert.assertEquals(State.CONFIRMED, adventure.getState());
+	}
+
+	@Test
+	public void successSequenceTwo(@Mocked final BankInterface bankInterface,
+			@Mocked final ActivityInterface activityInterface, @Mocked final HotelInterface roomInterface) {
+		new StrictExpectations() {
+			{
+				BankInterface.processPayment(IBAN, AMOUNT);
+				this.result = PAYMENT_CONFIRMATION;
+
+				ActivityInterface.reserveActivity(arrival, arrival, AGE);
+				this.result = ACTIVITY_CONFIRMATION;
+
+				BankInterface.getOperationData(PAYMENT_CONFIRMATION);
+
+				ActivityInterface.getActivityReservationData(ACTIVITY_CONFIRMATION);
+			}
+		};
+
+		Adventure adventure = new Adventure(this.broker, arrival, arrival, AGE, IBAN, AMOUNT);
+
+		adventure.process();
+		adventure.process();
+		adventure.process();
+
+		Assert.assertEquals(State.CONFIRMED, adventure.getState());
+	}
+
+}
