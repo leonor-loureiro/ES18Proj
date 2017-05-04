0078ee19d5444b5194fac0abf0cf7df2e4ebe3a0
António Rito Silva <Rito.Silva@ist.utl.pt>
2017-05-04 10:28:24 +0100

    tune script to run 200 concurrent threads

--- hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/Application.java
+++ hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/Application.java
@@ -1,11 +1,33 @@
 package pt.ulisboa.tecnico.softeng.hotel;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import pt.ist.fenixframework.Atomic;
+import pt.ist.fenixframework.FenixFramework;
+import pt.ulisboa.tecnico.softeng.hotel.domain.Hotel;
+import pt.ulisboa.tecnico.softeng.hotel.domain.Room;
+import pt.ulisboa.tecnico.softeng.hotel.domain.Room.Type;
+
 @SpringBootApplication
-public class Application {
+public class Application implements InitializingBean {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Atomic
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		Hotel hotel = FenixFramework.getDomainRoot().getHotelSet().stream().filter(h -> h.getCode().equals("HT12345"))
+				.findFirst().orElse(null);
+		if (hotel == null) {
+			hotel = new Hotel("HT12345", "XPTO");
+
+			for (int i = 0; i < 10000; i++) {
+				new Room(hotel, Integer.toString(i), Type.SINGLE);
+			}
+		}
+
 	}
 }
