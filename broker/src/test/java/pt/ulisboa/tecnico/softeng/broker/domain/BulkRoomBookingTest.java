feae16e9b779d7a92221c06e7a0f3584d7215ba7
António Rito Silva <Rito.Silva@ist.utl.pt>
2018-02-13 21:14:38 +0000

    add tearDown

--- broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/BulkRoomBookingTest.java
+++ broker/src/test/java/pt/ulisboa/tecnico/softeng/broker/domain/BulkRoomBookingTest.java
@@ -1,58 +1,64 @@
 package pt.ulisboa.tecnico.softeng.broker.domain;
 
-import org.joda.time.LocalDate;
-import org.junit.Before;
-import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
+import org.joda.time.LocalDate;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class BulkRoomBookingTest {
 
-    private final LocalDate begin = new LocalDate(2016, 12, 19);
-    private final LocalDate end = new LocalDate(2016, 12, 21);
+	private final LocalDate begin = new LocalDate(2016, 12, 19);
+	private final LocalDate end = new LocalDate(2016, 12, 21);
 
-    private Broker broker;
+	private Broker broker;
 
-    @Before
-    public void setUp() {
-        Broker.brokers.clear();
+	@Before
+	public void setUp() {
+		Broker.brokers.clear();
 
-        broker = new Broker("BR01", "WeExplore");
-    }
+		this.broker = new Broker("BR01", "WeExplore");
+	}
 
-    @Test
-    public void success() {
-        BulkRoomBooking bulk = new BulkRoomBooking(2, begin, end);
+	@Test
+	public void success() {
+		BulkRoomBooking bulk = new BulkRoomBooking(2, this.begin, this.end);
 
-        assertEquals(0, bulk.getReferences().size());
-        assertEquals(2, bulk.getNumber());
-        assertEquals(begin, bulk.getArrival());
-        assertEquals(end, bulk.getDeparture());
-    }
+		assertEquals(0, bulk.getReferences().size());
+		assertEquals(2, bulk.getNumber());
+		assertEquals(this.begin, bulk.getArrival());
+		assertEquals(this.end, bulk.getDeparture());
+	}
 
-    @Test
-    public void processBulkBooking() {
-        BulkRoomBooking bulk = new BulkRoomBooking(2, begin, end);
+	@Test
+	public void processBulkBooking() {
+		BulkRoomBooking bulk = new BulkRoomBooking(2, this.begin, this.end);
 
-        bulk.processBooking();
+		bulk.processBooking();
 
-        assertEquals(0, bulk.getReferences().size());
-        assertEquals(2, bulk.getNumber());
-        assertEquals(begin, bulk.getArrival());
-        assertEquals(end, bulk.getDeparture());
-    }
+		assertEquals(0, bulk.getReferences().size());
+		assertEquals(2, bulk.getNumber());
+		assertEquals(this.begin, bulk.getArrival());
+		assertEquals(this.end, bulk.getDeparture());
+	}
 
-    @Test
-    public void getRefBooking() {
-        BulkRoomBooking bulk = new BulkRoomBooking(2, begin, end);
+	@Test
+	public void getRefBooking() {
+		BulkRoomBooking bulk = new BulkRoomBooking(2, this.begin, this.end);
 
-        bulk.processBooking();
+		bulk.processBooking();
 
-        assertEquals(0, bulk.getReferences().size());
-        assertEquals(2, bulk.getNumber());
-        assertEquals(begin, bulk.getArrival());
-        assertEquals(end, bulk.getDeparture());
-        assertNull(bulk.getReference("CR7"));
-    }
+		assertEquals(0, bulk.getReferences().size());
+		assertEquals(2, bulk.getNumber());
+		assertEquals(this.begin, bulk.getArrival());
+		assertEquals(this.end, bulk.getDeparture());
+		assertNull(bulk.getReference("CR7"));
+	}
+
+	@After
+	public void tearDown() {
+		Broker.brokers.clear();
+	}
 }
