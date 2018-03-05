6fde4cf6752ff4a1ee414e14d83930b9424a07d1
wimpytoaster <antonio.terra@tecnico.ulisboa.pt>
2018-03-05 18:24:37 +0000

    Create test classes empty #17

---
+++ tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/ItemTypeConstructorTest.java
@@ -0,0 +1,29 @@
+package pt.ulisboa.tecnico.softeng.tax.domain;
+
+import org.joda.time.LocalDate;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ItemTypeConstructorTest {
+
+	@Before
+	public void setUp() {
+		
+	}
+
+	@Test
+	public void success() {
+		
+	}
+
+
+
+	@After
+	public void tearDown() {
+		
+	}
+
+}
