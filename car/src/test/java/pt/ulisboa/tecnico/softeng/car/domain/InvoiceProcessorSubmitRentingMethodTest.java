27fe8aaa9989f0cfa4751ebeed79867fd5851cb8
CarrascoII <tomas.carrasco@tecnico.ulisboa.pt>
2018-04-06 17:27:34 +0100

    Implemented tests for managing invoice queue in car and hotel. #54

--- car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/InvoiceProcessorSubmitRentingMethodTest.java
+++ car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/InvoiceProcessorSubmitRentingMethodTest.java
@@ -10,9 +10,12 @@ import mockit.Expectations;
 import mockit.FullVerifications;
 import mockit.Mocked;
 import mockit.integration.junit4.JMockit;
-import pt.ulisboa.tecnico.softeng.tax.dataobjects.InvoiceData;
+import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
+import pt.ulisboa.tecnico.softeng.car.exception.RemoteAccessException;
 import pt.ulisboa.tecnico.softeng.car.interfaces.BankInterface;
 import pt.ulisboa.tecnico.softeng.car.interfaces.TaxInterface;
+import pt.ulisboa.tecnico.softeng.tax.dataobjects.InvoiceData;
+import pt.ulisboa.tecnico.softeng.tax.exception.TaxException;
 
 
 @RunWith(JMockit.class)
@@ -22,12 +25,19 @@ public class InvoiceProcessorSubmitRentingMethodTest {
 	private RentACar rentACar;
 	private Vehicle vehicle;
 	private Renting renting;
+	
+	private static final String clientNIF = "135792468";
+	private static final String clientIBAN = "ES063";
+	private static final String drivingLicence = "br112233";
 
+	private static final String INVOICE_REFERENCE = "InvoiceReference";
+	private static final String PAYMENT_REFERENCE = "PaymentReference";
+	
 	@Before
 	public void setUp() {
 		this.rentACar = new RentACar("Oasis", "123456789", "123456");
 		this.vehicle = new Car("22-33-HZ", 50, 50, this.rentACar);
-		this.renting = new Renting("br112233", begin, end, this.vehicle, "987654321", "654321");		
+		this.renting = new Renting(drivingLicence, begin, end, this.vehicle, clientNIF, clientIBAN);		
 	}
 
 	@Test
@@ -43,6 +53,102 @@ public class InvoiceProcessorSubmitRentingMethodTest {
 
 		new FullVerifications() {
 			{
+			}
+		};
+	}
+	
+	@Test
+	public void oneTaxFailureOnSubmitInvoice(@Mocked final TaxInterface taxInterface,
+			@Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				this.result = PAYMENT_REFERENCE;
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				this.result = new TaxException();
+				this.result = INVOICE_REFERENCE;
+			}
+		};
+
+		this.rentACar.getProcessor().submitRenting(this.renting);
+		this.rentACar.getProcessor().submitRenting(new Renting(drivingLicence, begin, end, this.vehicle, clientNIF, clientIBAN));
+
+		new FullVerifications(taxInterface) {
+			{
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				this.times = 3;
+			}
+		};
+	}
+
+	@Test
+	public void oneRemoteFailureOnSubmitInvoice(@Mocked final TaxInterface taxInterface,
+			@Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				this.result = PAYMENT_REFERENCE;
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				this.result = new RemoteAccessException();
+				this.result = INVOICE_REFERENCE;
+			}
+		};
+
+		this.rentACar.getProcessor().submitRenting(this.renting);
+		this.rentACar.getProcessor().submitRenting(new Renting(drivingLicence, begin, end, this.vehicle, clientNIF, clientIBAN));
+
+		new FullVerifications(taxInterface) {
+			{
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				this.times = 3;
+			}
+		};
+	}
+
+	@Test
+	public void oneBankFailureOnProcessPayment(@Mocked final TaxInterface taxInterface,
+			@Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				this.result = new BankException();
+				this.result = PAYMENT_REFERENCE;
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				this.result = INVOICE_REFERENCE;
+			}
+		};
+
+		this.rentACar.getProcessor().submitRenting(this.renting);
+		this.rentACar.getProcessor().submitRenting(new Renting(drivingLicence, begin, end, this.vehicle, clientNIF, clientIBAN));
+
+		new FullVerifications(bankInterface) {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				this.times = 3;
+			}
+		};
+	}
+
+	@Test
+	public void oneRemoteFailureOnProcessPayment(@Mocked final TaxInterface taxInterface,
+			@Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				this.result = new RemoteAccessException();
+				this.result = PAYMENT_REFERENCE;
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				this.result = INVOICE_REFERENCE;
+			}
+		};
+
+		this.rentACar.getProcessor().submitRenting(this.renting);
+		this.rentACar.getProcessor().submitRenting(new Renting(drivingLicence, begin, end, this.vehicle, clientNIF, clientIBAN));
+
+		new FullVerifications(bankInterface) {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				this.times = 3;
 			}
 		};
 	}
--- hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/InvoiceProcessorSubmitBookingMethodTest.java
+++ hotel/src/test/java/pt/ulisboa/tecnico/softeng/hotel/domain/InvoiceProcessorSubmitBookingMethodTest.java
@@ -11,6 +11,10 @@ import mockit.FullVerifications;
 import mockit.Mocked;
 import mockit.integration.junit4.JMockit;
 import pt.ulisboa.tecnico.softeng.tax.dataobjects.InvoiceData;
+import pt.ulisboa.tecnico.softeng.tax.exception.TaxException;
+import pt.ulisboa.tecnico.softeng.hotel.domain.Booking;
+import pt.ulisboa.tecnico.softeng.hotel.exception.RemoteAccessException;
+import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
 import pt.ulisboa.tecnico.softeng.hotel.interfaces.BankInterface;
 import pt.ulisboa.tecnico.softeng.hotel.interfaces.TaxInterface;
 
@@ -24,11 +28,16 @@ public class InvoiceProcessorSubmitBookingMethodTest {
 	
 	private static final int singlePRICE = 10;
 	private static final int doublePRICE = 10;
+	private static final String clientNIF = "135792468";
+	private static final String clientIBAN = "ES063";
+	
+	private static final String INVOICE_REFERENCE = "InvoiceReference";
+	private static final String PAYMENT_REFERENCE = "PaymentReference";
 	
 	@Before
 	public void setUp() {
 		this.hotel = new Hotel("1234567", "Caravela", "123456789", "123456", singlePRICE, doublePRICE);
-		this.booking = new Booking(this.hotel, arrival, departure, "987654321", "654321");		
+		this.booking = new Booking(this.hotel, arrival, departure, clientNIF, clientIBAN);		
 	}
 
 	@Test
@@ -69,7 +78,101 @@ public class InvoiceProcessorSubmitBookingMethodTest {
 		};
 	}
 	
-	
+	@Test
+	public void oneTaxFailureOnSubmitInvoice(@Mocked final TaxInterface taxInterface,
+			@Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				this.result = PAYMENT_REFERENCE;
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				this.result = new TaxException();
+				this.result = INVOICE_REFERENCE;
+			}
+		};
+
+		this.hotel.getProcessor().submitBooking(this.booking);
+		this.hotel.getProcessor().submitBooking(new Booking(this.hotel, arrival, departure, clientNIF, clientIBAN));
+
+		new FullVerifications(taxInterface) {
+			{
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				this.times = 3;
+			}
+		};
+	}
+
+	@Test
+	public void oneRemoteFailureOnSubmitInvoice(@Mocked final TaxInterface taxInterface,
+			@Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				this.result = PAYMENT_REFERENCE;
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				this.result = new RemoteAccessException();
+				this.result = INVOICE_REFERENCE;
+			}
+		};
+
+		this.hotel.getProcessor().submitBooking(this.booking);
+		this.hotel.getProcessor().submitBooking(new Booking(this.hotel, arrival, departure, clientNIF, clientIBAN));
+
+		new FullVerifications(taxInterface) {
+			{
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				this.times = 3;
+			}
+		};
+	}
+
+	@Test
+	public void oneBankFailureOnProcessPayment(@Mocked final TaxInterface taxInterface,
+			@Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				this.result = new BankException();
+				this.result = PAYMENT_REFERENCE;
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				this.result = INVOICE_REFERENCE;
+			}
+		};
+
+		this.hotel.getProcessor().submitBooking(this.booking);
+		this.hotel.getProcessor().submitBooking(new Booking(this.hotel, arrival, departure, clientNIF, clientIBAN));
+
+		new FullVerifications(bankInterface) {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				this.times = 3;
+			}
+		};
+	}
+
+	@Test
+	public void oneRemoteFailureOnProcessPayment(@Mocked final TaxInterface taxInterface,
+			@Mocked final BankInterface bankInterface) {
+		new Expectations() {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				this.result = new RemoteAccessException();
+				this.result = PAYMENT_REFERENCE;
+				TaxInterface.submitInvoice((InvoiceData) this.any);
+				this.result = INVOICE_REFERENCE;
+			}
+		};
+
+		this.hotel.getProcessor().submitBooking(this.booking);
+		this.hotel.getProcessor().submitBooking(new Booking(this.hotel, arrival, departure, clientNIF, clientIBAN));
+
+		new FullVerifications(bankInterface) {
+			{
+				BankInterface.processPayment(this.anyString, this.anyDouble);
+				this.times = 3;
+			}
+		};
+	}
 	
 	@After
 	public void tearDown() {
