8ceee8dd58b995b820ddb85a634442dc52b3e6a2
dfAndrade <diogo.freire.dandrade@gmail.com>
2018-04-07 13:28:30 +0100

    Added required functions and removed unused ones. closes #63

--- broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/interfaces/CarInterface.java
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/interfaces/CarInterface.java
@@ -1,21 +1,18 @@
 package pt.ulisboa.tecnico.softeng.broker.interfaces;
 
-import java.util.Set;
-
 import org.joda.time.LocalDate;
 
 import pt.ulisboa.tecnico.softeng.car.dataobjects.RentingData;
 import pt.ulisboa.tecnico.softeng.car.domain.RentACar;
 import pt.ulisboa.tecnico.softeng.car.domain.Renting;
-import pt.ulisboa.tecnico.softeng.car.domain.Vehicle;
 
-public interface CarInterface {
-	public static Set<Vehicle> getAllAvailableMotorcycles(LocalDate begin, LocalDate end) {
-		return RentACar.getAllAvailableMotorcycles(begin, end);
+public class CarInterface {
+	public static String rentVehicle(Class<?> cls, String driving_licence, LocalDate begin, LocalDate end, String clientNIF, String clientIBAN) {
+		return RentACar.rentVehicle(cls, driving_licence, begin, end, clientNIF, clientIBAN);
 	}
-	
-	public static Set<Vehicle> getAllAvailableCars(LocalDate begin, LocalDate end) {
-		return RentACar.getAllAvailableCars(begin, end);
+
+	public static String cancelRenting(String reference) {
+		return RentACar.cancelRenting(reference);
 	}
 	
 	public static Renting getRenting(String reference) {
