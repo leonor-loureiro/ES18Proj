5250e2fa41c54f409dd0958242fdac845b6212a3
António Rito Silva <Rito.Silva@ist.utl.pt>
2018-04-07 12:32:53 +0100

    complete persistence test of module car

--- car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/CarPersistenceTest.java
+++ car/src/test/java/pt/ulisboa/tecnico/softeng/car/domain/CarPersistenceTest.java
@@ -16,6 +16,7 @@ import pt.ist.fenixframework.FenixFramework;
 public class CarPersistenceTest {
 	private static final String NAME1 = "eartz";
 	private static final String PLATE_CAR1 = "aa-00-11";
+	private static final String PLATE_CAR2 = "aa-00-12";
 	private static final String DRIVING_LICENSE = "br123";
 	private static final LocalDate date1 = LocalDate.parse("2018-01-06");
 	private static final LocalDate date2 = LocalDate.parse("2018-01-07");
@@ -33,6 +34,7 @@ public class CarPersistenceTest {
 	public void atomicProcess() {
 		RentACar rentACar = new RentACar(NAME1, NIF, IBAN);
 		Car car = new Car(PLATE_CAR1, 10, 10, rentACar);
+		Motorcycle motorcycle = new Motorcycle(PLATE_CAR2, 20, 5, rentACar);
 		car.rent(DRIVING_LICENSE, date1, date2, NIF, IBAN_BUYER);
 	}
 
@@ -41,7 +43,7 @@ public class CarPersistenceTest {
 		assertEquals(1, FenixFramework.getDomainRoot().getRentACarSet().size());
 
 		RentACar rentACar = new ArrayList<>(FenixFramework.getDomainRoot().getRentACarSet()).get(0);
-		assertEquals(1, rentACar.getVehicleSet().size());
+		assertEquals(2, rentACar.getVehicleSet().size());
 		Processor processor = rentACar.getProcessor();
 		assertEquals(NAME1, rentACar.getName());
 		assertEquals(NIF, rentACar.getNif());
@@ -49,19 +51,34 @@ public class CarPersistenceTest {
 		assertNotNull(processor);
 		assertEquals(1, processor.getRentingSet().size());
 
-		Vehicle car = new ArrayList<>(rentACar.getVehicleSet()).get(0);
-		assertEquals(PLATE_CAR1, car.getPlate());
-		assertEquals(10, car.getKilometers());
-		assertEquals(10, car.getPrice(), 0);
+		for (Vehicle vehicle : rentACar.getVehicleSet()) {
+			if (vehicle instanceof Car) {
+				assertEquals(PLATE_CAR1, vehicle.getPlate());
+				assertEquals(10, vehicle.getKilometers());
+				assertEquals(10, vehicle.getPrice(), 0);
+			}
+			if (vehicle instanceof Motorcycle) {
+				assertEquals(PLATE_CAR2, vehicle.getPlate());
+				assertEquals(20, vehicle.getKilometers());
+				assertEquals(5, vehicle.getPrice(), 0);
+			}
+		}
 
-		assertEquals(1, car.getRentingSet().size());
-		Renting renting = new ArrayList<>(car.getRentingSet()).get(0);
-		assertEquals(DRIVING_LICENSE, renting.getDrivingLicense());
-		assertEquals(date1, renting.getBegin());
-		assertEquals(date2, renting.getEnd());
-		assertEquals(NIF, renting.getClientNif());
-		assertEquals(IBAN, renting.getClientIban());
-		assertEquals(processor, renting.getProcessor());
+		for (Vehicle vehicle : rentACar.getVehicleSet()) {
+			if (vehicle instanceof Car) {
+				assertEquals(1, vehicle.getRentingSet().size());
+				Renting renting = new ArrayList<>(vehicle.getRentingSet()).get(0);
+				assertEquals(DRIVING_LICENSE, renting.getDrivingLicense());
+				assertEquals(date1, renting.getBegin());
+				assertEquals(date2, renting.getEnd());
+				assertEquals(NIF, renting.getClientNif());
+				assertEquals(IBAN, renting.getClientIban());
+				assertEquals(processor, renting.getProcessor());
+			}
+			if (vehicle instanceof Motorcycle) {
+				assertEquals(0, vehicle.getRentingSet().size());
+			}
+		}
 	}
 
 	@After
