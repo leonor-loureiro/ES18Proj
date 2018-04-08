59061fd1384ceac4ef0bae87f5c5cf2fbaa6f213
Anfifo <afonseca-1995@hotmail.com>
2018-04-08 19:50:15 +0100

    #46 submits invoice to tax module in state payment

--- broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/ProcessPaymentState.java
+++ broker/src/main/java/pt/ulisboa/tecnico/softeng/broker/domain/ProcessPaymentState.java
@@ -4,10 +4,11 @@ import org.joda.time.LocalDate;
 
 import pt.ulisboa.tecnico.softeng.bank.exception.BankException;
 import pt.ulisboa.tecnico.softeng.broker.domain.Adventure.State;
+import pt.ulisboa.tecnico.softeng.broker.exception.RemoteAccessException;
 import pt.ulisboa.tecnico.softeng.broker.interfaces.BankInterface;
 import pt.ulisboa.tecnico.softeng.broker.interfaces.TaxInterface;
-import pt.ulisboa.tecnico.softeng.broker.exception.RemoteAccessException;
 import pt.ulisboa.tecnico.softeng.tax.dataobjects.InvoiceData;
+import pt.ulisboa.tecnico.softeng.tax.exception.TaxException;
 
 public class ProcessPaymentState extends AdventureState {
 	public static final int MAX_REMOTE_ERRORS = 3;
@@ -23,8 +24,12 @@ public class ProcessPaymentState extends AdventureState {
 			
 			adventure.updateAmount();
 			adventure.setPaymentConfirmation(BankInterface.processPayment(adventure.getClientIBAN(), adventure.getAmount()));
-			
-			
+			try {
+				InvoiceData invoice = new InvoiceData(adventure.getBroker().getNIFSeller(), adventure.getClientIBAN(), "Servico", adventure.getAmount(), LocalDate.now());
+				TaxInterface.submitInvoice(invoice);		
+			} catch (TaxException te) {
+				//implement handling of exception
+			}
 		} catch (BankException be) {
 			adventure.setState(State.UNDO);
 			return;
