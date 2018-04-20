9a378971d796e5fe968f2cb962dc3ad5922c8372
António Rito Silva <Rito.Silva@ist.utl.pt>
2018-04-20 18:54:07 +0100

    closes #184

---
+++ tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/presentation/IrsController.java
@@ -0,0 +1,13 @@
+package pt.ulisboa.tecnico.softeng.tax.presentation;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/tax")
+public class IrsController {
+	private static Logger logger = LoggerFactory.getLogger(IrsController.class);
+
+}
