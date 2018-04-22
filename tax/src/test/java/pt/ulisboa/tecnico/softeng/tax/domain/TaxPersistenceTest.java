ca4fba85335a793d72ad5a6d501536f4d647705d
CarrascoII <tomas.carrasco@tecnico.ulisboa.pt>
2018-04-22 13:30:42 +0100

    Created test that tests tax module's persistency. #131

--- tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/TaxPersistenceTest.java
+++ tax/src/test/java/pt/ulisboa/tecnico/softeng/tax/domain/TaxPersistenceTest.java
@@ -1,19 +1,20 @@
 package pt.ulisboa.tecnico.softeng.tax.domain;
 
-import org.joda.time.LocalDate;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.joda.time.LocalDate;
 import org.junit.After;
 import org.junit.Test;
 
 import pt.ist.fenixframework.Atomic;
 import pt.ist.fenixframework.Atomic.TxMode;
 import pt.ist.fenixframework.FenixFramework;
-
-import java.util.ArrayList;
-import java.util.List;
+import pt.ulisboa.tecnico.softeng.tax.dataobjects.InvoiceData;
 
 public class TaxPersistenceTest {
 
@@ -28,9 +29,11 @@ public class TaxPersistenceTest {
     private static final String SELLER_NAME = "Tozerino";
     private static final String SELLER_ADDRESS = "Rua das Couves, No. 2";
     
-    private static final double INVOICE_VALUE = 100;
-    private static final LocalDate INVOICE_DATE = new LocalDate();
-
+    private static final double INVOICE_VALUE1 = 100;
+    private static final LocalDate INVOICE_DATE1 = new LocalDate(2018,5,4);
+    
+    private static final double INVOICE_VALUE2 = 50;
+    private static final LocalDate INVOICE_DATE2 = new LocalDate(2018,5,5);
 
     @Test
     public void success() {
@@ -45,8 +48,12 @@ public class TaxPersistenceTest {
 
         Buyer buyer = new Buyer(irs, BUYER_NIF, BUYER_NAME, BUYER_ADDRESS);
         Seller seller = new Seller(irs, SELLER_NIF, SELLER_NAME, SELLER_ADDRESS);
-        
-        Invoice invoice = new Invoice(INVOICE_VALUE, INVOICE_DATE, itemType, seller, buyer);
+        		
+        Invoice invoice = new Invoice(INVOICE_VALUE1, INVOICE_DATE1, itemType, seller, buyer);
+        		
+        InvoiceData invoiceData = new InvoiceData(SELLER_NIF, BUYER_NIF, ITEMTYPE_NAME, INVOICE_VALUE2, INVOICE_DATE2);
+        String invoiceRef = IRS.submitInvoice(invoiceData);
+        IRS.cancelInvoice(invoiceRef);
     }
 
     @Atomic(mode = TxMode.READ)
@@ -60,6 +67,7 @@ public class TaxPersistenceTest {
 
         assertEquals(ITEMTYPE_NAME, itemType.getName());
         assertEquals(ITEMTYPE_TAX, itemType.getTax());
+        assertEquals(irs.getItemTypeByName(ITEMTYPE_NAME), itemType);
 
         List<TaxPayer> taxPayers = new ArrayList<>(irs.getTaxPayerSet());
         TaxPayer taxPayer = taxPayers.get(1);
@@ -67,17 +75,33 @@ public class TaxPersistenceTest {
         assertEquals(BUYER_NIF, taxPayer.getNIF());
         assertEquals(BUYER_NAME, taxPayer.getName());
         assertEquals(BUYER_ADDRESS, taxPayer.getAddress());
-
+        assertEquals(irs.getTaxPayerByNIF(BUYER_NIF), taxPayer);
+        
+        Buyer buyer = (Buyer) taxPayer;
         TaxPayer taxPayer1 = taxPayers.get(0);
 
         assertEquals(SELLER_NIF, taxPayer1.getNIF());
         assertEquals(SELLER_NAME, taxPayer1.getName());
         assertEquals(SELLER_ADDRESS, taxPayer1.getAddress());
-
-
-
-
-
+        assertEquals(irs.getTaxPayerByNIF(SELLER_NIF), taxPayer1);
+        
+        Seller seller = (Seller) taxPayer1;
+        List<Invoice> buyerInvoices = new ArrayList<>(buyer.getInvoiceSet());
+        Invoice invoice0 = buyerInvoices.get(0);
+        
+        assertEquals((int) INVOICE_VALUE1, (int) invoice0.getValue());
+        assertEquals(INVOICE_DATE1, invoice0.getDate());
+        
+        List<Invoice> sellerInvoices = new ArrayList<>(seller.getInvoiceSet());
+        Invoice invoice1 = sellerInvoices.get(0);
+        
+        assertEquals((int) invoice1.getValue(), (int) invoice0.getValue());
+        assertEquals(invoice1.getDate(), invoice0.getDate());
+        
+        Invoice invoice2 = sellerInvoices.get(1);
+        
+        assertEquals(invoice2.getDate(), INVOICE_DATE2);
+        assertTrue(invoice2.getCancelled());
     }
 
     @After
