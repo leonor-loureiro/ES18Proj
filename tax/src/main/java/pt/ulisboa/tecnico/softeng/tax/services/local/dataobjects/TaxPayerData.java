3504a1f11e30ddae03d883d7529ca1e3dc7fd5b0
António Rito Silva <Rito.Silva@ist.utl.pt>
2018-04-22 12:52:48 +0100

    closes #190

--- tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/services/local/dataobjects/TaxPayerData.java
+++ tax/src/main/java/pt/ulisboa/tecnico/softeng/tax/services/local/dataobjects/TaxPayerData.java
@@ -1,6 +1,11 @@
 package pt.ulisboa.tecnico.softeng.tax.services.local.dataobjects;
 
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
+
 import pt.ulisboa.tecnico.softeng.tax.domain.Buyer;
+import pt.ulisboa.tecnico.softeng.tax.domain.Seller;
 import pt.ulisboa.tecnico.softeng.tax.domain.TaxPayer;
 
 public class TaxPayerData {
@@ -12,15 +17,27 @@ public class TaxPayerData {
 	private String name;
 	private String address;
 	private Type type;
+	private Map<Integer, Double> taxes = new TreeMap<Integer, Double>();
 
 	public TaxPayerData() {
 	}
 
 	public TaxPayerData(TaxPayer taxPayer) {
+		Seller seller;
+		Buyer buyer;
 		this.nif = taxPayer.getNif();
 		this.name = taxPayer.getName();
 		this.address = taxPayer.getAddress();
 		this.type = taxPayer instanceof Buyer ? Type.BUYER : Type.SELLER;
+		if (taxPayer instanceof Seller) {
+			seller = (Seller) taxPayer;
+			this.taxes = seller.getInvoiceSet().stream().map(i -> i.getDate().getYear()).distinct()
+					.collect(Collectors.toMap(y -> y, y -> seller.toPay(y)));
+		} else {
+			buyer = (Buyer) taxPayer;
+			this.taxes = buyer.getInvoiceSet().stream().map(i -> i.getDate().getYear()).distinct()
+					.collect(Collectors.toMap(y -> y, y -> buyer.taxReturn(y)));
+		}
 	}
 
 	public String getName() {
@@ -53,6 +70,14 @@ public class TaxPayerData {
 
 	public void setType(Type type) {
 		this.type = type;
+	}
+
+	public Map<Integer, Double> getTaxes() {
+		return this.taxes;
+	}
+
+	public void setTaxes(Map<Integer, Double> taxes) {
+		this.taxes = taxes;
 	}
 
 }
