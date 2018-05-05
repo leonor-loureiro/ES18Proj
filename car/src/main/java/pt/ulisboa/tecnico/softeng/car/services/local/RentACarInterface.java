14a7c732222d395d691a7b0c60aa548eb134336c
dfAdnrade <diogo.freire.dandrade@gmail.com>
2018-05-05 02:53:02 +0100

    Added RentACar UI, now creates RentACars #158
     - RentACarInterface
     - RentACarController
     - RentACarData
     - rentACars.html

--- car/src/main/java/pt/ulisboa/tecnico/softeng/car/services/local/RentACarInterface.java
+++ car/src/main/java/pt/ulisboa/tecnico/softeng/car/services/local/RentACarInterface.java
@@ -1,4 +1,13 @@
 package pt.ulisboa.tecnico.softeng.car.services.local;
 
+import pt.ist.fenixframework.Atomic;
+import pt.ist.fenixframework.Atomic.TxMode;
+import pt.ulisboa.tecnico.softeng.car.domain.RentACar;
+import pt.ulisboa.tecnico.softeng.car.services.local.dataobjects.RentACarData;
+
 public class RentACarInterface {
+    @Atomic(mode = TxMode.WRITE)
+    public static void createRentACar(RentACarData rentacar) {
+        new RentACar(rentacar.getName(), rentacar.getNif(), rentacar.getIban());
+    }
 }
--- car/src/main/java/pt/ulisboa/tecnico/softeng/car/services/local/dataobjects/RentACarData.java
+++ car/src/main/java/pt/ulisboa/tecnico/softeng/car/services/local/dataobjects/RentACarData.java
@@ -1,4 +1,42 @@
 package pt.ulisboa.tecnico.softeng.car.services.local.dataobjects;
 
+import pt.ulisboa.tecnico.softeng.car.domain.RentACar;
+
 public class RentACarData {
+    private String name;
+    private String nif;
+    private String iban;
+
+    public RentACarData() {
+    }
+
+    public RentACarData(RentACar rentACar) {
+        this.name = rentACar.getName();
+        this.nif  = rentACar.getNif();
+        this.iban = rentACar.getIban();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
 }
