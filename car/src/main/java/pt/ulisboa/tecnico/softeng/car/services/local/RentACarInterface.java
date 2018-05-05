40f2015b10e3ad7feffc47c4af087824c5af8964
dfAdnrade <diogo.freire.dandrade@gmail.com>
2018-05-06 00:18:27 +0100

    Added Vehicles' UI, now creates vehicles #160
     - Fixed link in rentACar's listings

--- car/src/main/java/pt/ulisboa/tecnico/softeng/car/services/local/RentACarInterface.java
+++ car/src/main/java/pt/ulisboa/tecnico/softeng/car/services/local/RentACarInterface.java
@@ -3,9 +3,13 @@ package pt.ulisboa.tecnico.softeng.car.services.local;
 import pt.ist.fenixframework.Atomic;
 import pt.ist.fenixframework.Atomic.TxMode;
 import pt.ist.fenixframework.FenixFramework;
+import pt.ulisboa.tecnico.softeng.car.domain.Car;
+import pt.ulisboa.tecnico.softeng.car.domain.Motorcycle;
 import pt.ulisboa.tecnico.softeng.car.domain.RentACar;
 import pt.ulisboa.tecnico.softeng.car.domain.RentACar_Base;
+import pt.ulisboa.tecnico.softeng.car.exception.CarException;
 import pt.ulisboa.tecnico.softeng.car.services.local.dataobjects.RentACarData;
+import pt.ulisboa.tecnico.softeng.car.services.local.dataobjects.VehicleData;
 
 import java.util.Comparator;
 import java.util.List;
@@ -23,4 +27,38 @@ public class RentACarInterface {
                 .sorted(Comparator.comparing(RentACar_Base::getName))
                 .map(RentACarData::new).collect(Collectors.toList());
     }
+
+    @Atomic(mode = TxMode.READ)
+    public static RentACarData getRentACarDataByCode(String code) {
+        RentACar rac = getRentACarByCode(code);
+        if (rac == null)
+            return null;
+        return new RentACarData(rac);
+    }
+
+    @Atomic(mode = TxMode.WRITE)
+    public static void createVehicle(String code, VehicleData vd) {
+        RentACar rac = getRentACarByCode(code);
+        if (rac == null || vd.getIsCar() == null)
+            throw new CarException();
+
+        switch(vd.getIsCar()) {
+            case "Motorcycle":
+                new Motorcycle(vd.getPlate(), vd.getKilometers(), vd.getPrice(), rac);
+                break;
+            case "Car":
+                new Car(vd.getPlate(), vd.getKilometers(), vd.getPrice(), rac);
+                break;
+            default:
+                throw new CarException();
+        }
+    }
+
+    private static RentACar getRentACarByCode(String code) {
+        return FenixFramework.getDomainRoot().getRentACarSet().stream()
+                .filter(a -> a.getCode().equals(code))
+                .findFirst().orElse(null);
+    }
+
+
 }
