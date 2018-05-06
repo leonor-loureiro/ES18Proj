fe22df65cbe885541fb45cff15b3003b713ed353
Anfifo <afonseca-1995@hotmail.com>
2018-05-06 18:16:22 +0100

    fixed persistency tests conflicts with remote accesses

--- hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/services/remote/TaxInterface.java
+++ hotel/src/main/java/pt/ulisboa/tecnico/softeng/hotel/services/remote/TaxInterface.java
@@ -1,29 +1,40 @@
 package pt.ulisboa.tecnico.softeng.hotel.services.remote;
 
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
 import pt.ulisboa.tecnico.softeng.hotel.services.remote.dataobjects.InvoiceData;
+import pt.ulisboa.tecnico.softeng.hotel.services.remote.exceptions.RemoteAccessException;
 
 import java.util.Map;
 import java.util.TreeMap;
 
 public class TaxInterface {
 	public static String submitInvoice(InvoiceData invoiceData) {
+		try{
 
-		String url = "http://localhost:8086/taxpayersControl/submitInvoice";
-		RestTemplate rest = new RestTemplate();
+			String url = "http://localhost:8086/taxpayersControl/submitInvoice";
+			RestTemplate rest = new RestTemplate();
 
-		return rest.postForObject(url, invoiceData, String.class);
+			return rest.postForObject(url, invoiceData, String.class);
+		}catch(ResourceAccessException e){
+			throw new RemoteAccessException(e.getMessage());
+		}
 	}
 
 	public static void cancelInvoice(String reference) {
 
-		String url = "http://localhost:8086/taxpayersControl/cancelInvoice/{reference}";
+		try {
+			String url = "http://localhost:8086/taxpayersControl/cancelInvoice/{reference}";
 
-		RestTemplate rest = new RestTemplate();
+			RestTemplate rest = new RestTemplate();
 
-		Map<String, Object> map = new TreeMap<>();
-		map.put("reference", reference);
+			Map<String, Object> map = new TreeMap<>();
+			map.put("reference", reference);
 
-		rest.getForObject(url, String.class, map);
+			rest.getForObject(url, String.class, map);
+		}
+		catch (ResourceAccessException e){
+			throw new RemoteAccessException(e.getMessage());
+		}
 	}
 }
