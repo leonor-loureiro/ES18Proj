b955bcebcb90aaf9697c6a2090b2394aa2d527df
Anfifo <afonseca-1995@hotmail.com>
2018-05-06 16:29:06 +0100

    #177 implemented rest invocations from activity to tax and bank

--- activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/services/remote/TaxInterface.java
+++ activity/src/main/java/pt/ulisboa/tecnico/softeng/activity/services/remote/TaxInterface.java
@@ -1,16 +1,29 @@
 package pt.ulisboa.tecnico.softeng.activity.services.remote;
 
+import org.springframework.web.client.RestTemplate;
 import pt.ulisboa.tecnico.softeng.activity.services.remote.dataobjects.InvoiceData;
+
+import java.util.Map;
+import java.util.TreeMap;
 
 public class TaxInterface {
 	public static String submitInvoice(InvoiceData invoiceData) {
-		// return IRS.submitInvoice(invoiceData);
-		// TODO: implement in the final version as a rest invocation
-		return null;
+
+		String url = "http://localhost:8086/taxpayersControl/submitInvoice";
+		RestTemplate rest = new RestTemplate();
+
+		return rest.postForObject(url, invoiceData, String.class);
 	}
 
-	public static void cancelInvoice(String invoiceReference) {
-		// IRS.cancelInvoice(invoiceReference);
-		// TODO: implement in the final version as a rest invocation
+	public static void cancelInvoice(String reference) {
+
+		String url = "http://localhost:8086/taxpayersControl/cancelInvoice/{reference}";
+
+		RestTemplate rest = new RestTemplate();
+
+		Map<String, Object> map = new TreeMap<>();
+		map.put("reference", reference);
+
+		rest.getForObject(url, String.class, map);
 	}
 }
