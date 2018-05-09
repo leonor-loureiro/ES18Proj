7b94dbb1b7fc7cca1e3a41b427268ea6ccc547da
Anfifo <afonseca-1995@hotmail.com>
2018-05-09 09:17:09 +0100

    added startservers script for windows

---
+++ bin/winstartservers.bat
@@ -0,0 +1,6 @@
+cd ..\tax & start mvn clean spring-boot:run
+cd ..\bank & start mvn clean spring-boot:run
+cd ..\hotel & start mvn clean spring-boot:run
+cd ..\activity & start mvn clean spring-boot:run
+cd ..\car & start mvn clean spring-boot:run
+cd ..\broker & start mvn clean spring-boot:run
